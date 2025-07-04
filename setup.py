6ef0306c4cc2ab639e59d938fb729f2cdf6d9c2a
PRINCY SONI <soniprincy02@gmail.com>
2025-07-04 10:53:57 +0530

    Fix setup.py and requirements for deployment

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
+# import setuptools
+
+# with open("README.md", "r", encoding="utf-8") as f:
+#     long_description = f.read()
+
+
+# __version__ = "0.0.0"
+
+# REPO_NAME = "Chicken-Disease-Classification--Project"
+# AUTHOR_USER_NAME = "Soniprincy"
+# SRC_REPO = "cnnClassifier"
+# AUTHOR_EMAIL = "soniprincy02@gmail.com"
+
+
+# setuptools.setup(
+#     name=SRC_REPO,
+#     version=__version__,
+#     author=AUTHOR_USER_NAME,
+#     author_email=AUTHOR_EMAIL,
+#     description="A small python package for CNN app",
+#     long_description=long_description,
+#     long_description_content="text/markdown",
+#     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+#     project_urls={
+#         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+#     },
+#     package_dir={"": "src"},
+#     packages=setuptools.find_packages(where="src")
+# )
+
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
-
 
 __version__ = "0.0.0"
 
@@ -10,7 +39,6 @@ REPO_NAME = "Chicken-Disease-Classification--Project"
 AUTHOR_USER_NAME = "Soniprincy"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "soniprincy02@gmail.com"
-
 
 setuptools.setup(
     name=SRC_REPO,
@@ -25,5 +53,24 @@ setuptools.setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "tensorflow",
+        "pandas",
+        "dvc",
+        "notebook",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "python-box==6.0.2",
+        "pyYAML",
+        "tqdm",
+        "ensure==1.0.2",
+        "joblib",
+        "types-PyYAML",
+        "scipy",
+        "Flask",
+        "Flask-Cors",
+        "streamlit"
+    ],
 )
